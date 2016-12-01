d16734474b8844622eb004d46327fa44a727fe58
unknown <w47593@PC78253.we.dirsrv.com>
2016-12-01 15:44:40 -0600

    new functions

---
+++ R/Static_Missing_Vars.R
@@ -0,0 +1,44 @@
+
+
+
+
+Static_Missing_Vars <- function(df) {
+  
+  list <- c()
+  removal <- c()
+  for(i in 1:length(colnames(df))){
+    
+    # Agg data
+    Cases = table(df[colnames(df)[i]])
+    Cases = sort(Cases,decreasing=T)
+    
+    # Drop Variables that are > 75% missing
+    if(sum(is.na(df[colnames(df)[i]]))/nrow(df) >=.75)
+    {
+      Percent = round(sum(is.na(df[colnames(df)[i]]))/nrow(df),2)
+      if(Percent ==1){Percent ='100'}
+      list = c(list , paste(Percent,"% of the data is NA in variable: ",colnames(df)[i],sep=''))
+      removal <- c(removal,colnames(df)[i])
+    }
+    # Drop variables that are close to static (Warning sometimes the minroity help explain variance)  
+    else if(Cases[1]/nrow(df)>.95)
+    {
+      Percent = round(Cases[1]/nrow(df),2)
+      if(Percent ==1){Percent ='100'}
+      list = c(list , paste(Percent,"% of the data is Static in variable: ",colnames(df)[i],sep=''))
+      removal <- c(removal,colnames(df)[i])
+    }
+    else if(Cases[1]/nrow(df)>.8)
+    {
+      print(paste('Variable',colnames(df)[i],'was not dropped, but',Cases[1]/nrow(df),'of the cases are static'))
+    }
+  }
+  if(length(list)>0){
+    print('The following variables will be dropped')
+    print(as.data.frame(list))
+    df = df[, !(colnames(df) %in% c((removal)))]
+  }
+  
+  return(df)
+}   
+data <- Static_Missing_Vars(iris)
